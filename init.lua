ca575ceecad7b160db16aeafc1d7b64adced311f
adidev18 <alagar.18@proton.me>
2023-09-03 19:11:51 +0530

    AdidevVim#2

--- init.lua
+++ init.lua
@@ -93,24 +93,24 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+  -- {
+  -- Autocompletion
+  --'hrsh7th/nvim-cmp',
+  --dependencies = {
+  -- Snippet Engine & its associated nvim-cmp source
+  --'L3MON4D3/LuaSnip',
+  --'saadparwaiz1/cmp_luasnip',
 
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
+  -- Adds LSP completion capabilities
+  --'hrsh7th/cmp-nvim-lsp',
 
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
+  -- Adds a number of user-friendly snippets
+  --'rafamadriz/friendly-snippets',
+  --},
+  --},
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} },
+  -- { 'folke/which-key.nvim',          opts = {} },
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -145,19 +145,19 @@ require('lazy').setup({
 
   },
 
-  {
-    -- lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+  --{
+  -- lualine as statusline
+  --'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  --opts = {
+  --options = {
+  --icons_enabled = false,
+  --theme = 'auto',
+  --component_separators = '|',
+  -- section_separators = '',
+  -- },
+  -- },
+  --},
 
   {
     -- Add indentation guides even on blank lines
@@ -228,10 +228,19 @@ require('lazy').setup({
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.auto_pairs',
   require 'kickstart.plugins.code_runner',
-  require 'kickstart.plugins.flash',
   require 'kickstart.plugins.executor',
   require 'kickstart.plugins.sniprun',
   require 'kickstart.plugins.dashboard',
+  require 'kickstart.plugins.cmp',
+  require 'kickstart.plugins.icons',
+  require 'kickstart.plugins.flash',
+  require 'kickstart.plugins.indent',
+  require 'kickstart.plugins.fileman',
+  require 'kickstart.plugins.lualine',
+  require 'kickstart.plugins.comment',
+  require 'kickstart.plugins.whick-key',
+  require 'kickstart.plugins.project',
+  require 'kickstart.plugins.bufferline',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -479,8 +488,8 @@ local servers = {
 require('neodev').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+--capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
@@ -501,51 +510,6 @@ mason_lspconfig.setup_handlers {
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
